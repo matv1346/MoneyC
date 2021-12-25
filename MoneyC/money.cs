339da872da8facb5acfbe7084003b7bdb37c3936
matv <78199275+matv1346@users.noreply.github.com>
2021-12-25 17:48:18 +0300

    add

--- MoneyC/money.cs
+++ MoneyC/money.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+class Money
+{
+    public string inputstr;
+    public char[] a;
+    public void input()
+    {
+        inputstr = Interaction.InputBox("Введите все оценки", "Ввод оценок");
+        a = inputstr.ToCharArray();
+        
+
+    }
+}
+
+class moneymain
+{
+    static void Main()
+    {
+       string a;
+        a = Interaction.InputBox("Напишите 'averenge' если хотите узнать средний балл или 'money' что бы узнать количество денег", "Что вы хотите?");
+        if (a == "money")
+        {
+            Money A = new Money();
+            A.input();
+
+        }
+
+        else if(a == "averenge")
+        {
+
+        }
+        else
+        {
+            MessageBox.Show("Error1 введено неверное значение", "Ошибка!");
+        }
+    }
+}
